37fbda8eb9a83c2bca1924c6f8275eba3567dd56
Mohd Altamash <mohdaltamash37986@gmail.com>
2026-08-11 20:09:47 +0530

    Add: majority-element - submission-9

---
+++ Data Structures & Algorithms/majority-element/submission-9.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+     HashMap<Integer,Integer> map = new HashMap<>();
+
+     int result = 0;
+
+     for(int num:nums){
+        map.put(num,map.getOrDefault(num,0)+1);
+
+
+        if(map.get(num)> nums.length/2){
+            result = num;
+            
+        }
+     }   
+     return result;
+    }
+}
