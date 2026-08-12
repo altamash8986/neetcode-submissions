3adbf5cb659fe8fc5d612eea4588a6ad109ae2fb
Mohd Altamash <mohdaltamash37986@gmail.com>
2026-08-12 18:36:52 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] asteroidCollision(int[] nums) {
+
+     Stack<Integer> stack = new Stack<>();
+
+     for(int num: nums ){
+        while(!stack.isEmpty() && num< 0 && stack.peek()> 0){
+            int diff = num + stack.peek();
+
+            if(diff< 0){
+                stack.pop();
+            }
+            else if(diff > 0){
+                num = 0;
+            }
+            else{
+                num = 0;
+                stack.pop();
+            }
+        }
+        if(num!=0){
+            stack.add(num);
+        }
+     }   
+     return stack.stream().mapToInt(i -> i).toArray();
+    }
+}
