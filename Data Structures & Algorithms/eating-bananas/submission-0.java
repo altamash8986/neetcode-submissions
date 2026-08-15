9930e0e57a2f166dee33697bbaa963e52163b19f
Mohd Altamash <mohdaltamash37986@gmail.com>
2026-08-15 17:14:24 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int minEatingSpeed(int[] nums, int h) {
+        int left = 1;
+        int right = Arrays.stream(nums).max().getAsInt();
+
+        int result = right;
+
+        while(left<=right){
+            int index = (left+ right)/2;
+
+            long time = 0;
+
+            for(int num:nums){
+                time +=Math.ceil((double) num/index );
+            }
+
+            if(time<=h){
+                result = index;
+
+                right = index -1;
+            }
+            else{
+                left = index+1;
+            }
+        }
+return result;
+    }
+}
